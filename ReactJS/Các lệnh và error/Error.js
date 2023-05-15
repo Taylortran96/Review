ed7e0cbec5a53693c295e46844e35b5a4d732dab
Taylortran96 <tranaithy96@gmail.com>
2023-05-15 12:57:11 +0700

    Update Error.js

--- ReactJS/Các lệnh và error/Error.js
+++ ReactJS/Các lệnh và error/Error.js
@@ -1,12 +1,30 @@
-// Trong command
+//1. Trong command
     //1. Check node.js có trên máy: Cmd => node -v && nmp -v 
     //2. Đọc các thông tin, tìm hiểu thêm các câu lệnh node.js: cmd => node -h
     //3. 
 
-// Cài đặt thư viện
-    //
+//2. Cài đặt thư viện
+    //1. Icon: FontAwesome
+        //A. Cài cmd
+            npm install --save @fortawesome/fontawesome-svg-core
+            npm install --save @fortawesome/free-solid-svg-icons
+            npm install --save @fortawesome/react-fontawesome
 
-// Trong Visual Studio code : Terminal (Ctrl + Shift + `)
+        //B.Import
+            import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+            import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+        //C. trong function
+            <FontAwesomeIcon icon={faThumbsUp} />
+    //2. Icon: ReactIcon
+        // A. Cài cmd
+            npm install react-icons --save
+            npm install @react-icons/all-files --save
+        // B.Import
+            import { FaBeer } from 'react-icons/fa';
+            import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
+        //
+
+//3.Trong Visual Studio code : Terminal (Ctrl + Shift + `)
     //1. Cài đặt tool: npm install -g create-react-app 
     //2. Tạo ứng dụng React: npx create-react-app my-react-app (my-react-app là tên folder)
     //3. Chạy ứng dung: cd my-react-app && npm start
@@ -22,7 +40,7 @@
         // right-click vào link and end-process
     //5.Lối "ERROR in Plugin "react" was conflicted between "package.json » eslint-config-react-app »..." => Vào "package.JSON" => Ctrl + S
     //6. Cách mở reactjs trên trình duyệt web => terminal => cd tới folder react => Nhập "npm start"
-    //7.
+    //7. Cách tạo package.json => npm init -y or npm install express
     //8.
     //9.
     //10.
